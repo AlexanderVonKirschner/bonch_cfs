0712307328f8dabcb6574c997c389fd132a394ff
AlexanderVonMuller <100016689+AlexanderVonMuller@users.noreply.github.com>
2022-10-01 01:39:18 +0300

    Update test2_5.sh

--- test2_5.sh
+++ test2_5.sh
@@ -3,14 +3,14 @@
 quit_flag=0
 
 break_the_loop() {
-    echo "SIGINT trapped"
+    echo "SIGINT trapped" >& 2 # DEBUG
     quit_flag=1
 }
 
 trap break_the_loop INT
 
 while [ $quit_flag -ne 1 ] ; do
-    echo "this is the main loop"
+    echo "this is the main loop" >& 2 # DEBUG
     sleep 1
 done
 
