5fbfbab37b6f5bd52c79c16e64dc8e57b024d6c1
AlexanderVonMuller <100016689+AlexanderVonMuller@users.noreply.github.com>
2022-09-30 23:49:52 +0300

    Create test2_6.sh

---
+++ test2_6.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+    echo "Usage: $0 <file>" >& 2
+    exit 1
+fi
+
+sighup_handler() {
+    echo "SIGHUP trapped" >& 2 # DEBUG
+    {
+        sleep 30
+        kill $$
+    } &
+}
+
+trap sighup_handler HUP
+
+while true ; do
+    time=`date +"%T"`
+    echo $time > $1
+    echo $time >& 2 # DEBUG
+    sleep 10
+done
