74a908dde4878202a383b54519737e749da4b486
AlexanderVonMuller <100016689+AlexanderVonMuller@users.noreply.github.com>
2022-10-10 23:00:12 +0300

    Add files via upload

---
+++ lib_example/lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+print_green_string()
+{
+    echo -e "\x1b[32m$1\x1b[0m"
+}
---
+++ lib_example/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source $HOME/cps/lib.sh
+
+if [ $# -eq 1 ] ; then
+    print_green_string "$1"
+fi
